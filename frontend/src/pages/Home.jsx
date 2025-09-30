7c3a1e44781ee7d19f454b1a40aee7f3775c4d38
netra405 <netrarajbanshi18@gmail.com>
2025-09-30 13:22:15 +0545

    home section frontend complete , collection too

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
 
-const home = () => {
+
+const Home = () => {
   return (
     <div>
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
+      <NewsletterBox />
      
     </div>
   )
 }
 
-export default home
+export default Home

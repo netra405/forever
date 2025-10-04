d0cf46e5d002a935d70868e3edcb5be1fd0c1378
netra405 <netrarajbanshi18@gmail.com>
2025-10-04 17:59:23 +0545

    almost frontend is  completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,25 +12,28 @@ import Orders from './pages/Orders.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import SearchBar from './components/SearchBar.jsx'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Navbar/>
-      <SearchBar/>
+      <ToastContainer/>
+      <Navbar />
+      <SearchBar />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/collection' element={<Collection/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contect' element={<Contact/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/produtc/:produuctId' element={<Product/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/palce-order' element={<PlaceOrder/>} />
-        <Route path='/orders' element={<Orders/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contect' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
       </Routes>
-      <Footer/>
+      <Footer />
 
     </div>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
-import { ShopContext } from '../context/shopContext';
+import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
 
     const [visible, setVisible] = useState(false);
-    const {setShowSearch} = useContext(ShopContext); 
+    const {setShowSearch, getCartCount} = useContext(ShopContext); 
 
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
@@ -38,7 +38,7 @@ const Navbar = () => {
                 <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
                 <div className='group relative'>
-                    <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+                <Link to='/login'><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -49,7 +49,7 @@ const Navbar = () => {
                 </div>
                 <Link to='/cart' className='relative'>
                 <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
-                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
                 </Link>
                 <img onClick={()=> {
                     setVisible(true)
@@ -57,7 +57,7 @@ const Navbar = () => {
             </div>
 
             {/* sidebar menu for small screen */}
-            <div className={`absolute top-0 right-0 bottom-0 overflow-hhidden bg-white treansition-all duration-300 ${visible ? 'w-full' : 'w-0'}`}>
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white treansition-all duration-300 ${visible ? 'w-full' : 'w-0'}`}>
                 <div className='flex flex-col text-gray-600 '>
                     <div onClick={()=>setVisible(false)} className='cursor-pointer flex items-center gap-4 p-3'>
                         <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="logo" />
